6126917559e86cfee732de14ef93e8661c10cf8e
Stefan Seidl <stefan.famseidl@aon.at>
2021-02-08 23:52:18 +0100

    new tester method

--- funktionenHandler/Tester.java
+++ funktionenHandler/Tester.java
@@ -1,16 +1,34 @@
 package funktionenHandler;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Tester {
 
 	public static void main(String[] args) {
 		
-				 
+		System.out.println(lexerTest());
 	
+	
+	}
+	
+	
+	public static boolean lexerTest()
+	{
+		String s = "4 * x ^ 3 + (3 + x)^2 * 3";
+		
+		ArrayList<String> gelext = Lexer.lexen(s);
+		
+		String[] as = {"4","*","x","^", "3", "+", "(","3","+","x", ")", "^", "2", "*", "3"};
+		
+		List<String> test = Arrays.asList(as);
+		
+		if(gelext.equals(test)) return true;
+		return false;
 		
 		
-		Funktion f = new Funktion("4*x^2 + (3*2)^x");
-		System.out.println(f.berechneFür(2));
+		
 	}
 }
